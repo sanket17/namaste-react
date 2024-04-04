97ca2b77c3a66f02459cda1aeaa9ed04c4653e7d
sanket17-amazatic <sanket.t@amazatic.com>
2024-04-04 16:51:35 +0530

    Inception

---
+++ App.js
@@ -0,0 +1,29 @@
+// /**
+//  * <div id='parent'>
+//  *  <div id="child">
+//  *         <h1></h1>
+//  * <h2></h2>
+//  * </div>
+//  * </div>
+//  * 
+//  * 
+//  * 
+//  */
+
+
+
+
+// const h1 = React.createElement('h1', {id: 'heading'}, 'Namaste React!!');
+// const h2 = React.createElement('h2', {id: 'heading'}, 'I am a H2 tag');
+// const child = React.createElement('div', {id: 'child'}, [h1, h2]);
+// const parent = React.createElement('div', {id: 'parent'}, child);
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// // console.log(root);
+// root.render(parent);
+
+// const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+// // console.log(root);
+// root1.render(parent);
+
+
