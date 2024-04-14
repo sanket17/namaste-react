8d74523f43882556b38ec1a4a22bd18d10bc75d9
sanket17-amazatic <sanket.t@amazatic.com>
2024-04-14 22:48:39 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -5,25 +5,23 @@
 //  * <h2></h2>
 //  * </div>
 //  * </div>
-//  * 
-//  * 
-//  * 
+//  *
+//  *
+//  *
 //  */
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HeaderComponent from './HeaderComponent';
+import './style.css';
 
-
-
-// const h1 = React.createElement('h1', {id: 'heading'}, 'Namaste React!!');
-// const h2 = React.createElement('h2', {id: 'heading'}, 'I am a H2 tag');
-// const child = React.createElement('div', {id: 'child'}, [h1, h2]);
-// const parent = React.createElement('div', {id: 'parent'}, child);
-// console.log(parent);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// // console.log(root);
-// root.render(parent);
-
-// const root1 = ReactDOM.createRoot(document.getElementById('root1'));
-// // console.log(root);
-// root1.render(parent);
-
-
+const heading = (
+  <>
+    <HeaderComponent />
+    <h1>Namaste React</h1>
+  </>
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// console.log(root);
+root.render(heading);
